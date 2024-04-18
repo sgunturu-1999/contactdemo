dc37c6e97ddfb3e0f130e805146f5a55391a27b7
sgunturu-1999 <srikanth.gunturugsk@gmail.com>
2024-04-18 20:39:51 +0530

    Vite+Yarn+TypeScript template

---
+++ contactdemo/src/App.tsx
@@ -0,0 +1,20 @@
+
+import  ContactForm  from './components/ContactForm';
+import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+
+function App() {
+  return (<></>
+    // <Router>
+    //   <div>
+    //     <Switch>
+    //       <Route path="/" Component={ContactList} />
+    //       <Route path="/contacts/new" Component={ContactForm} />
+    //       <Route path="/categories" Component={CategoryList} />
+          
+    //     </Switch>
+    //   </div>
+    // </Router>
+  );
+}
+
+export default App
